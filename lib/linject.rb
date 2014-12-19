b2873a6e4b37fdfd942fd7bf5143697179596696
Luke Clewlow <luke.clewlow@gmail.com>
2014-12-19 15:07:26 +0000

    can now take block and arguemnts

--- lib/linject.rb
+++ lib/linject.rb
@@ -1,12 +1,20 @@
 class Array
 
-	def linject(x =nil)
-		copy = self.dup 
-		sum = copy.shift
-		copy.each do |item|
-		sum = yield sum, item
-		end
-		sum
+	def linject(x=nil)
+		if x == nil
+			copy = self.dup 
+			sum = copy.shift
+			copy.each do |item|
+			sum = yield sum, item
+			end
+			sum
+		else
+			copy = self.dup 
+			sum = x
+			copy.each do |item|
+			sum = yield sum, item
+			end
+			sum
+		end	
 	end
-
 end	
